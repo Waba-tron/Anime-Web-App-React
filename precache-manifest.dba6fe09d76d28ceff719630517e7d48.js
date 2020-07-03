ebfd358db8fb65296c3e46904247aa11582dd797
Wahab Rehman <wahab_rehman1256@outlook.com>
2020-07-03 18:20:35 +0100

    Updates

---
+++ precache-manifest.dba6fe09d76d28ceff719630517e7d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c582d83b42d4b69a5a814bd84c6581",
+    "url": "/Anime-Web-App-React/index.html"
+  },
+  {
+    "revision": "1f774cc04ea4b454f9e6",
+    "url": "/Anime-Web-App-React/static/css/main.16836b94.chunk.css"
+  },
+  {
+    "revision": "533f91608be88f7b0d6c",
+    "url": "/Anime-Web-App-React/static/js/2.e329b3e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Anime-Web-App-React/static/js/2.e329b3e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f774cc04ea4b454f9e6",
+    "url": "/Anime-Web-App-React/static/js/main.1603bce0.chunk.js"
+  },
+  {
+    "revision": "c66f1f379ca64f09b98c",
+    "url": "/Anime-Web-App-React/static/js/runtime-main.e4ac6197.js"
+  }
+]);
